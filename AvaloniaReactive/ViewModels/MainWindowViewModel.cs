bb8b6178c1f1ec196ec4928df22862a469b01d5d
Gehongyan <gehongyan1996@126.com>
2023-07-31 22:42:52 +0800

    完善说明

--- AvaloniaReactive/ViewModels/MainWindowViewModel.cs
+++ AvaloniaReactive/ViewModels/MainWindowViewModel.cs
@@ -25,6 +25,9 @@ public class MainWindowViewModel : ViewModelBase
 {
     #region Binding to IObservable
 
+    /// <summary>
+    ///     绑定到 IObservable，需要使用脱字符号（^）。这种方式不需要使用 [Reactive] 特性。
+    /// </summary>
     public IObservable<long> SecondsFromStartup =>
         Observable.Interval(TimeSpan.FromSeconds(1))
             .StartWith(0);
@@ -33,6 +36,9 @@ public class MainWindowViewModel : ViewModelBase
 
     #region Binding via ObservableAsPropertyHelper
 
+    /// <summary>
+    ///     在 WPF 等不支持直接绑定到 IObservable 的框架中，可以使用 ObservableAsPropertyHelper 来实现绑定。
+    /// </summary>
     private readonly ObservableAsPropertyHelper<long> _secondsFromHelper;
 
     public long SecondsFromHelper => _secondsFromHelper.Value;
@@ -49,11 +55,20 @@ public class MainWindowViewModel : ViewModelBase
 
     #region IObservable Basic
 
+    /// <summary>
+    ///     此处模拟一个搜索框，当输入内容时，会触发搜索，用于演示 IObservable 的一个实际的用法。
+    /// </summary>
     [Reactive]
     public string? SearchQuery { get; set; }
 
+    /// <summary>
+    ///     这是一个将 NuGet 搜索结果进行扩展的类，用于在 UI 中显示。
+    /// </summary>
     public record PackageSearchMetadataViewData(IPackageSearchMetadata Metadata)
     {
+        /// <summary>
+        ///     该命令包装了项目的 URL，用于在浏览器中打开。
+        /// </summary>
         public ICommand OpenPage => ReactiveCommand.Create(() =>
         {
             Process.Start(new ProcessStartInfo
@@ -63,25 +78,25 @@ public class MainWindowViewModel : ViewModelBase
             });
         });
 
-        public Task<IImage> IconImage
+        /// <summary>
+        ///     该任务包装了通过网络请求下载图标的任务，Avalonia 可以直接绑定到任务，使用脱字符号（^）。
+        /// </summary>
+        public Task<IImage> IconImage => Task.Run(async () =>
         {
-            get
-            {
-                return Task.Run(async () =>
-                {
-                    await Task.Yield();
-                    HttpClient httpClient = new();
-                    Stream dataStream = await httpClient.GetStreamAsync(Metadata.IconUrl);
-                    MemoryStream memStream = new();
-                    await dataStream.CopyToAsync(memStream);
-                    memStream.Position = 0;
-                    IImage newBitmap = new Bitmap(memStream);
-                    return newBitmap;
-                });
-            }
-        }
+            await Task.Yield();
+            HttpClient httpClient = new();
+            Stream dataStream = await httpClient.GetStreamAsync(Metadata.IconUrl);
+            MemoryStream memStream = new();
+            await dataStream.CopyToAsync(memStream);
+            memStream.Position = 0;
+            IImage newBitmap = new Bitmap(memStream);
+            return newBitmap;
+        });
     }
 
+    /// <summary>
+    ///     当搜索框中的内容发生变化时，会触发搜索，搜索结果会被绑定到这个属性上。
+    /// </summary>
     public IObservable<IEnumerable<PackageSearchMetadataViewData>> SearchResults => this
         .WhenAnyValue(x => x.SearchQuery)
         .Throttle(TimeSpan.FromSeconds(1))
@@ -91,8 +106,13 @@ public class MainWindowViewModel : ViewModelBase
         .SelectMany(async x => await SearchNuGetPackagesAsync(x, CancellationToken.None))
         .Select(x => x.Take(4).Select(y => new PackageSearchMetadataViewData(y)));
 
-    private async Task<IEnumerable<IPackageSearchMetadata>> SearchNuGetPackagesAsync(
-        string? term, CancellationToken token)
+    /// <summary>
+    ///     搜索 NuGet 包。
+    /// </summary>
+    /// <param name="term">搜索关键字。</param>
+    /// <param name="token">取消令牌。</param>
+    /// <returns>搜索结果。</returns>
+    private async Task<IEnumerable<IPackageSearchMetadata>> SearchNuGetPackagesAsync(string? term, CancellationToken token)
     {
         List<Lazy<INuGetResourceProvider>> providers = new();
         providers.AddRange(Repository.Provider.GetCoreV3());
@@ -108,23 +128,51 @@ public class MainWindowViewModel : ViewModelBase
 
     #region WhenAnyValue
 
+    /// <summary>
+    ///     输入框的文字所绑定到的属性。虽然不会被用于更新视图，但需要用于通知其他属性该属性的变更。
+    /// </summary>
+    /// <remarks>
+    ///     通过 IL Viewer，可以看到 [Reactive] 所生产的代码为：
+    ///     <code language="csharp">
+    ///         [Reactive]
+    ///         public string? InputText
+    ///         {
+    ///           get =&gt; this.$InputText;
+    ///           set =&gt; this.RaiseAndSetIfChanged&lt;MainWindowViewModel, string&gt;(ref this.$InputText, value, nameof (InputText));
+    ///         }
+    ///     </code>
+    ///     <see cref="IReactiveObjectExtensions.RaiseAndSetIfChanged{TObj,TRet}"/>
+    ///     方法内部调用了 <see cref="IReactiveObjectExtensions.RaisePropertyChanged{TSender}"/>，来通知其它属性该属性的变更。
+    /// </remarks>
     [Reactive]
     public string? InputText { get; set; }
 
+    /// <summary>
+    ///     当输入框中的内容发生变化时，会触发计算输入框中的字符数。
+    /// </summary>
     public IObservable<int> InputTextLength => this
         .WhenAnyValue(x => x.InputText)
         .Select(x => x?.Length ?? 0)
         .StartWith(InputText?.Length ?? 0);
 
+    /// <summary>
+    ///     当输入框中的内容发生变化时，会触发计算输入框中的数字数。
+    /// </summary>
     public IObservable<int> DigitCount => this
         .WhenAnyValue(x => x.InputText)
         .Select(x => x?.Count(char.IsDigit) ?? 0)
         .StartWith(InputText?.Length ?? 0);
 
+    /// <summary>
+    ///     当输入框中的内容发生变化时，会触发判断输入框中的内容是否可以被转换为 <see cref="DateTime"/> 类型。
+    /// </summary>
     public IObservable<bool> IsDateTime => this
         .WhenAnyValue(x => x.InputText)
         .Select(x => DateTime.TryParse(x, out _));
 
+    /// <summary>
+    ///     当输入框中的内容发生变化时，会触发判断输入框中的内容是否为手机号码。
+    /// </summary>
     public IObservable<bool> IsPhoneNumber => this
         .WhenAnyValue(x => x.InputText)
         .Select(x => x?.Length == 11 && x.All(char.IsDigit));
@@ -133,15 +181,28 @@ public class MainWindowViewModel : ViewModelBase
 
     #region Command canExecute
 
+    /// <summary>
+    ///     模拟用户是否同意用户协议。
+    /// </summary>
     [Reactive]
     public bool Agree { get; set; }
 
+    /// <summary>
+    ///     模拟用户名。
+    /// </summary>
     [Reactive]
     public string? Username { get; set; }
 
+    /// <summary>
+    ///     模拟密码。
+    /// </summary>
     [Reactive]
     public string? Password { get; set; }
 
+    /// <summary>
+    ///     通过一个简单的校验，为注册按钮的可用性提供支持。
+    ///     当用户同意用户协议、用户名不为空、密码长度在 6 到 32 之间时，注册按钮可用。
+    /// </summary>
     public ICommand RegisterCommand => ReactiveCommand.Create(() => { },
         canExecute: this.WhenAnyValue(
                 x => x.Agree,
@@ -156,6 +217,9 @@ public class MainWindowViewModel : ViewModelBase
 
     #region WhenAnyPropertyChanged
 
+    /// <summary>
+    ///     当 <see cref="Agree"/>、<see cref="Username"/>、<see cref="Password"/> 任意一个属性发生变化时，会触发计数。
+    /// </summary>
     public IObservable<int> ChangedPropertyTriggeredCount => this.WhenAnyPropertyChanged(
             nameof(Agree),
             nameof(Username),
@@ -167,6 +231,10 @@ public class MainWindowViewModel : ViewModelBase
 
     #region WhenAnyObservable
 
+    /// <summary>
+    ///     当 <see cref="SecondsFromStartup"/>、<see cref="InputTextLength"/>、<see cref="ChangedPropertyTriggeredCount"/>
+    ///     任意一个属性发生变化时，会触发计算，生成信息概要。
+    /// </summary>
     public IObservable<string> ObservableValuesSummary => this.WhenAnyObservable(
         x => x.SecondsFromStartup,
         x => x.InputTextLength,
@@ -177,11 +245,17 @@ public class MainWindowViewModel : ViewModelBase
 
     #region ObservableForProperty
 
+    /// <summary>
+    ///     当 <see cref="Password"/> 属性发生变化时，会触发计算，<c>beforeChange</c> 参数为 <c>true</c> 决定了变更触发前会被计算一次推送数据点。
+    /// </summary>
     public IObservable<string> ObservableForPropertyBefore => this.ObservableForProperty(x => x.Password, true)
         .Select(x => x.Value ?? string.Empty)
         .StartWith(Password ?? string.Empty);
 
-    public IObservable<string> ObservableForPropertyAfter => this.ObservableForProperty(x => x.Password)
+    /// <summary>
+    ///     当 <see cref="Password"/> 属性发生变化时，会触发计算，<c>beforeChange</c> 参数为 <c>false</c> 决定了变更触发后会被计算一次推送数据点。
+    /// </summary>
+    public IObservable<string> ObservableForPropertyAfter => this.ObservableForProperty(x => x.Password, false)
         .Select(x => x.Value ?? string.Empty)
         .StartWith(Password ?? string.Empty);
 
@@ -189,14 +263,35 @@ public class MainWindowViewModel : ViewModelBase
 
     #region ObservableCollection
 
+    /// <summary>
+    ///     模拟一群学生。
+    /// </summary>
     public ObservableCollection<Student> Students { get; } = new(Student.GetMany(5));
 
+    /// <summary>
+    ///     模拟添加学生。
+    /// </summary>
     public ICommand AppendStudentCommand => ReactiveCommand.Create(() => Students.Add(Student.GetOne(Students.Count)));
 
+    /// <summary>
+    ///     模拟移除学生。
+    /// </summary>
     public ICommand RemoveStudentCommand => ReactiveCommand.Create(() => Students.RemoveAt(Students.Count - 1));
 
+    /// <summary>
+    ///     计数学生数量，WhenAnyValue 是可以访问到属性的属性的。
+    /// </summary>
     public IObservable<int> StudentsCount => this.WhenAnyValue(x => x.Students.Count);
 
+    /// <summary>
+    ///     计算学生中年龄大于 20 的学生数量，对 ObservableCollection 调用 ToObservableChangeSet，
+    ///     可以将可观察集合转换为 IObservable&lt;IChangeSet&lt;T&gt;&gt;，
+    ///     即转换为集合的变更信息。通过 Filter 进行过滤，而非 Where。
+    ///     前者是对 IChangeSet 的泛型类型进行过滤，后者是对 IChangeSet 本身进行过滤。
+    ///     调用 ToCollection 方法将 IObservable&lt;IChangeSet&lt;T&gt;&gt;
+    ///     转换为 IObservable&lt;IReadOnlyCollection&lt;T&gt;&gt;，
+    ///     即将集合的变更集转换为完整的集合。最后通过 Select 计算集合的数量。
+    /// </summary>
     public IObservable<int> StudentsOlderThen20 => Students
         .ToObservableChangeSet()
         .Filter(x => x.Age > 20)
--- ReactiveTests/ObservableTests.cs
+++ ReactiveTests/ObservableTests.cs
@@ -13,6 +13,15 @@ public class ObservableTests
 
     #region Generation
 
+    /// <summary>
+    ///     该示例演示了通过 Subject 管理 IObservable 的基本用法。
+    ///     通过创建 Subject 作为 IObservable 对象，调用 OnNext、OnError 和 OnCompleted 方法来发出通知。
+    ///     通过调用 Subscribe 方法来订阅 Subject 来接收通知。
+    ///     OnNext 方法用于通知观察者，新的数据已经准备好了。
+    ///     OnError 方法用于通知观察者，发生了错误。
+    ///     OnCompleted 方法用于通知观察者，数据流已经结束。
+    ///     该示例中通过 Subject 推送了 int 类型 0、1、150、15000 四个数据点，一个错误和一个完成通知。
+    /// </summary>
     [Fact]
     public void Observable_Basic()
     {
@@ -36,6 +45,14 @@ public class ObservableTests
         Assert.Equal(150, value);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.FromEventPattern 创建 IObservable 的基本用法。
+    ///     通过 FromEventPattern 非泛型方法创建 IObservable 对象，
+    ///     方法的第一个参数是用于将 EventHandler&lt;TEventArgs&gt;
+    ///     委托转换为 NotifyCollectionChangedEventHandler 委托的类型转换。
+    ///     在所订阅的事件发生时，会向 IObservable 推送新的数据点。
+    ///     该示例中通过 FromEventPattern 方法推送了 NotifyCollectionChangedEventArgs。
+    /// </summary>
     [Fact]
     public void Observable_FromEventPattern()
     {
@@ -52,6 +69,12 @@ public class ObservableTests
         Assert.Equal(1, raisedCount);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.FromEventPattern 创建 IObservable 的基本用法。
+    ///     通过 FromEventPattern 泛型方法创建 IObservable 对象，
+    ///     在所订阅的事件发生时，会向 IObservable 推送新的数据点。
+    ///     该示例中通过 FromEventPattern 方法推送了 RoutedEventArgs。
+    /// </summary>
     [Fact]
     public void Observable_FromEventPatternGeneric()
     {
@@ -67,6 +90,12 @@ public class ObservableTests
         Assert.Equal(1, raisedCount);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 IEnumerable&lt;T&gt;.ToObservable 创建 IObservable 的基本用法。
+    ///     如需将非泛型 IEnumerable 转换为 IObservable，请先通过 Cast&lt;T&gt;
+    ///     或 OfType&lt;T&gt; 方法将其转换为泛型 IEnumerable&lt;T&gt;。
+    ///     该示例中通过 ToObservable 方法推送了 1 到 5 的整数序列。
+    /// </summary>
     [Fact]
     public void Observable_IEnumerable()
     {
@@ -77,6 +106,11 @@ public class ObservableTests
         Assert.True(Enumerable.Range(1, 5).SequenceEqual(values));
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Range 创建 IObservable 的基本用法。
+    ///     Observable.Range 方法用于创建一个 IObservable 对象，该对象会向观察者推送指定范围内的整数序列。
+    ///     该示例中通过 Range 方法推送了 1 到 5 的整数序列。
+    /// </summary>
     [Fact]
     public void Observable_Range()
     {
@@ -90,6 +124,11 @@ public class ObservableTests
 
     #region Restriction
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Where 限制 IObservable 的基本用法。
+    ///     Observable.Where 方法用于创建一个 IObservable 对象，该对象会向观察者推送满足指定条件的数据点。
+    ///     该示例中通过 Where 方法筛选出了小于 3 的数据点。
+    /// </summary>
     [Fact]
     public async Task Observable_Where_1()
     {
@@ -105,6 +144,11 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Where 限制 IObservable 的进阶用法。
+    ///     示例中通过 Observable.FromEventPattern 创建的时间参数序列中包含了多种类型的事件，
+    ///     通过 Where 方法筛选出了 NotifyCollectionChangedAction.Add 类型的事件。
+    /// </summary>
     [Fact]
     public void Observable_Where_2()
     {
@@ -127,6 +171,12 @@ public class ObservableTests
 
     #region Aggregation
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Aggregate 静态聚合 IObservable 的基本用法。
+    ///     Observable.Aggregate 方法用于创建一个 IObservable 对象，该对象会向观察者推送聚合后的数据点。
+    ///     该示例中通过 Aggregate 方法推送了 1 到 5 的整数序列依次聚合的和。
+    ///     Aggregate 方法要求 IObservable 序列已经完成，否则不会推送任何数据点。
+    /// </summary>
     [Fact]
     public void Observable_Aggregate()
     {
@@ -141,6 +191,12 @@ public class ObservableTests
         Assert.Equal(15, sum);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Scan 实时聚合 IObservable 的基本用法。
+    ///     Observable.Scan 方法用于创建一个 IObservable 对象，该对象会向观察者推送聚合后的数据点。
+    ///     该示例中通过 Scan 方法推送了 0 到 5 整数序列依次聚合的和。
+    ///     Scan 方法不要求 IObservable 序列已经完成，可以在序列推送数据点的同时推送聚合后的数据点。
+    /// </summary>
     [Fact]
     public async Task Observable_Scan()
     {
@@ -160,6 +216,11 @@ public class ObservableTests
 
     #region Time-based Generation
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Interval 定时创建 IObservable 的基本用法。
+    ///     Observable.Interval 方法用于创建一个 IObservable 对象，该对象会向观察者按照指定的时间间隔推送自然数序列。
+    ///     该示例中通过 Interval 方法推送了 0 到 4 的整数序列。
+    /// </summary>
     [Fact]
     public async Task Observable_Interval()
     {
@@ -175,6 +236,12 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Timer 计时器创建 IObservable 的基本用法。
+    ///     Observable.Timer 方法用于创建一个 IObservable 对象，该对象会向观察者在指定的初始化延迟后开始按照指定的时间间隔推送自然数序列。
+    ///     Observable.Timer 的第一个参数为初始化延迟，第二个参数为时间间隔。
+    ///     该示例中通过 Timer 方法推送了 0 到 3 的整数序列。
+    /// </summary>
     [Fact]
     public async Task Observable_Timer()
     {
@@ -190,6 +257,12 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Generate 创建 IObservable 的基本用法。
+    ///     Observable.Generate 方法用于创建一个 IObservable 对象，该对象会向观察者按照指定的时间间隔推送指定的序列。
+    ///     方法的第一个参数为初始值，第二个参数为判断条件，第三个参数为迭代器，第四个参数为推送的数据点，第五个参数为时间间隔。
+    ///     该示例中通过 Generate 方法推送了 0 到 9 的整数序列。
+    /// </summary>
     [Fact]
     public async Task Observable_Generate()
     {
@@ -213,6 +286,11 @@ public class ObservableTests
 
     #region Time-based Restriction
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Delay 延迟 IObservable 的基本用法。
+    ///     Observable.Delay 方法用于创建一个 IObservable 对象，该对象会向观察者推送延迟指定时间后的数据点。
+    ///     该示例中通过 Delay 方法将原始序列的数据点推送延迟了 2 秒。
+    /// </summary>
     [Fact]
     public async Task Observable_Delay()
     {
@@ -230,6 +308,11 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Throttle 节流 IObservable 的基本用法。
+    ///     Observable.Throttle 方法用于创建一个 IObservable 对象，该对象会向观察者推送指定时间间隔内的最后一个数据点。
+    ///     该示例中通过 Throttle 方法，将原始序列中与前一个数据点的时间间隔大于 500 毫秒的数据点推送给观察者。
+    /// </summary>
     [Fact]
     public async Task Observable_Throttle()
     {
@@ -251,6 +334,12 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Timeout 进行超时限制的 IObservable 的基本用法。
+    ///     Observable.Timeout 方法用于创建一个 IObservable 对象，该对象会向观察者推送原始序列的数据点，
+    ///     但是如果原始序列的数据点在指定的时间间隔内没有推送，则会向观察者推送 TimeoutException 异常。
+    ///     该示例中通过 Timeout 方法，在原始序列超过 3 秒没有推送数据点时，会向观察者推送 TimeoutException 异常。
+    /// </summary>
     [Fact]
     public async Task Observable_Timeout()
     {
@@ -274,6 +363,10 @@ public class ObservableTests
 
     #region Time-based Projection
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.TimeInterval 生成附带与前一数据点的时间间隔的 IObservable 的基本用法。
+    ///     Observable.TimeInterval 方法用于创建一个 IObservable 对象，该对象会向观察者推送包含原始数据点及与前一个数据点的时间间隔的数据点。
+    /// </summary>
     [Fact]
     public async Task Observable_TimeInterval()
     {
@@ -291,6 +384,10 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Timestamp 生成附带时间戳的 IObservable 的基本用法。
+    ///     Observable.Timestamp 方法用于创建一个 IObservable 对象，该对象会向观察者推送包含原始数据点及其时间戳的数据点。
+    /// </summary>
     [Fact]
     public async Task Observable_Timestamp()
     {
@@ -308,6 +405,10 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Sample 采样 IObservable 的基本用法。
+    ///     Observable.Sample 方法用于创建一个 IObservable 对象，该对象会向观察者推送原始序列中指定时间间隔内的最后一个数据点。
+    /// </summary>
     [Fact]
     public async Task Observable_Sample()
     {
@@ -324,6 +425,11 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Buffer 缓冲切分 IObservable 的基本用法。
+    ///     Observable.Buffer 方法用于创建一个 IObservable 对象，该对象会向观察者推送原始序列中按指定时间间隔分组的所有数据点。
+    ///     该示例中通过 Buffer 方法，每经过 5 秒，会将刚过去 5 秒内的数据点作为一个 IList 推送给观察者。
+    /// </summary>
     [Fact]
     public async Task Observable_Buffer()
     {
@@ -340,6 +446,11 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Window 窗口化切分 IObservable 的基本用法。
+    ///     Observable.Window 方法用于创建一个 IObservable 对象，该对象会向观察者推送原始序列中按指定时间间隔分组的子序列。
+    ///     该示例中通过 Window 方法，每 5 秒创建一个子序列，并将接下来 5 秒内的数据点推送给该子序列。
+    /// </summary>
     [Fact]
     public async Task Observable_Window()
     {
@@ -364,6 +475,9 @@ public class ObservableTests
 
     #region Background Processing
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Start 在后台线程执行代码的基本用法。
+    /// </summary>
     [Fact]
     public async Task Observable_RunCode()
     {
@@ -382,6 +496,9 @@ public class ObservableTests
         Assert.True(completed);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Start 在后台线程执行异步代码的基本用法。
+    /// </summary>
     [Fact]
     public async Task Observable_RunCodeAsynchronously()
     {
@@ -400,6 +517,10 @@ public class ObservableTests
         Assert.True(completed);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.CombineLatest 合并多个 IObservable 任务进行并行执行的基本用法。
+    ///     在 CombineLatest 方法中，每个 IObservable 任务都会在不同的线程中执行。
+    /// </summary>
     [Fact]
     public async Task Observable_ParallelExecution()
     {
@@ -431,6 +552,10 @@ public class ObservableTests
 
     #region Combinition
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Merge 合并多个 IObservable 的基本用法。
+    ///     在 Merge 中，每当任意一个 IObservable 推送新的数据点时，都只会将该数据点推送给观察者。
+    /// </summary>
     [Fact]
     public async Task Observable_Merge()
     {
@@ -451,6 +576,12 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Publish 将非共享 IObservable 转换为共享 IConnectableObservable 的基本用法。
+    ///     IObservable 可以被多个观察者订阅，每个观察者都会收到相同的数据序列。
+    ///     非共享 IObservable 在订阅后是立即开始推送数据的，因此示例中的 #1 #2 对于整个序列的处理事件是先后发生的。
+    ///     共享 IConnectableObservable 在订阅后需要调用 Connect 方法后才会开始推送数据，因此示例中的 #1 #2 的事件是以数据点为单位交替发生的。
+    /// </summary>
     [Fact]
     public async Task Observable_Publish()
     {
@@ -491,6 +622,12 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Zip 按位次合并多个 IObservable 的基本用法。
+    ///     在 Zip 中，每当所有 IObservable 都推送了新的数据点时，
+    ///     都会将所有 IObservable 的最新数据点通过 resultSelector 函数合并为一个数据点推送给观察者。
+    ///     实例中的 second 比 first 延迟了 500 毫秒，所以 first 一直都是在等待 second 推送数据后进行合并。
+    /// </summary>
     [Fact]
     public async Task Observable_Zip()
     {
@@ -511,6 +648,11 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.CombineLatest 合并多个 IObservable 最新值的基本用法。
+    ///     在 CombineLatest 中，每当任意一个 IObservable 推送新的数据点时，
+    ///     都会将所有 IObservable 的最新数据点通过 resultSelector 函数合并为一个数据点推送给观察者。
+    /// </summary>
     [Fact]
     public async Task Observable_CombineLatest()
     {
@@ -531,6 +673,11 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Concat 冷连接多个 IObservable 的基本用法。
+    ///     在 Concat 中，只有当前一个 IObservable 完成后，才会开始推送下一个 IObservable 的数据，
+    ///     且被连接的 IObservable 会在连接后再开始推送数据。
+    /// </summary>
     [Fact]
     public async Task Observable_Concat_Cold()
     {
@@ -558,6 +705,12 @@ public class ObservableTests
         Assert.NotEmpty(values);
     }
 
+    /// <summary>
+    ///     该示例演示了通过 Observable.Concat 热连接多个 IObservable 的基本用法。
+    ///     在 Concat 中，只有当前一个 IObservable 完成后，才会开始推送下一个 IObservable 的数据，
+    ///     但实例对 second 调用了 Publish 及 Connect，使其在连接前就开始推送数据，但此时还未有任何订阅者。
+    ///     当 first 完成后，连接的 second 所推送的数据点才开始被处理。
+    /// </summary>
     [Fact]
     public async Task Observable_Concat_Hot()
     {

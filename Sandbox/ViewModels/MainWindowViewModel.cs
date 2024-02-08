1fd47d8f7762415acc095674cc73bf1b9030035e
Gehongyan <gehongyan1996@126.com>
2024-02-08 09:47:57 +0800

    Clear sandbox

--- Sandbox/ViewModels/MainWindowViewModel.cs
+++ Sandbox/ViewModels/MainWindowViewModel.cs
@@ -1,47 +1,8 @@
-﻿using System.Collections.ObjectModel;
-using System.Linq;
-using System.Windows.Input;
-using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
-using Sandbox.Models;
-
-namespace Sandbox.ViewModels;
+﻿namespace Sandbox.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    /// <summary>
-    ///     性别
-    /// </summary>
-    [Reactive]
-    public Gender? IndividualGender { get; set; }
-
-    /// <summary>
-    ///     设置性别的命令
-    /// </summary>
-    public ICommand SetValueIndividuallyCommand => ReactiveCommand.Create<Gender>(gender => IndividualGender = gender);
-
-    /// <summary>
-    ///     性别
-    /// </summary>
-    [Reactive]
-    public Gender? UnifiedGender { get; set; }
-
-    /// <summary>
-    ///     设置性别的命令
-    /// </summary>
-    public ICommand SetValueUnifiedCommand => ReactiveCommand.Create<Gender>(gender => UnifiedGender = gender);
-
-    public ObservableCollection<SyncScrollDataGridItem> SyncScrollDataGridItemsSource =>
-        new(Enumerable.Range(1, 100).Select(x =>
-            new SyncScrollDataGridItem()
-            {
-                Name = $"Name {x}",
-                Value = $"Value {x}",
-            }));
-}
-
-public class SyncScrollDataGridItem
-{
-    public string? Name { get; set; }
-    public string? Value { get; set; }
+    public MainWindowViewModel()
+    {
+    }
 }
